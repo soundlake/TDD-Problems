ab79d06a7cd5edfd4af93ccbdbb36fc197c0e90f
Sinyeol An <soundlake@gmail.com>
2014-09-02 11:50:03 +0200

    make template

---
+++ template/test_solve.py
@@ -0,0 +1,9 @@
+import unittest
+from solve import something
+
+class test_something(unittest.TestCase):
+    def test_(self):
+        self.assertEqual()
+
+if __name__ == '__main__':
+    unittest.main()
