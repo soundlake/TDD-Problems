f17c8214f2ea347a6cc38376e451e54938a9dfb7
Sinyeol An <soundlake@gmail.com>
2014-09-04 21:56:17 +0200

    single variable expression

--- string-template_engine/solve.py
+++ string-template_engine/solve.py
@@ -1,7 +1,31 @@
+import re
+
+
+
 class map_of_vars:
-    def put(self, key, value):
-        pass
+    dictionary = dict()
+
+    @classmethod
+    def put(cls, key, value):
+        cls.dictionary[key] = value
+
+    @classmethod
+    def get(cls, key):
+        return cls.dictionary[key]
+
+
 
 class template_engine:
-    def evaluate(self, src, mov):
-        pass
+    pat_key = r'{\$(?P<key>.+)}'
+
+    @classmethod
+    def find_keys(cls, src):
+        mo = re.search(cls.pat_key, 'Hello {$name}')
+        print(mo.groups())
+        return mo.group('key')
+
+    @classmethod
+    def evaluate(cls, src, mov):
+        key = template_engine.find_keys(src)
+        val = map_of_vars.get(key)
+        return re.sub('{\$' + key + '}', val, src)
